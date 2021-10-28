286829f157e59f48ce35d54eb4308e21fc664bce
Illuminoirre <arrifqiagung@gmail.com>
2021-10-28 15:27:05 +0700

    laboratories
    uwes lengkap kuabeh

---
+++ resources/views/main/laboratories/energi.blade.php
@@ -0,0 +1,36 @@
+@extends("main.layouts.main")
+
+@section("content")
+<section id="laboratoire">
+    <img src="/svg/labo-wire.svg" class="labo-wire-top" alt="labo-wire-top">
+    <div class="container px-4 px-lg-0 pb-5">
+        <h1 class="labo-title text-center py-4">Laboratorium Rekayasa Energi dan Pengkondisian Lingkungan</h1>
+        <div class="row align-items-center">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="/img/lab-logo/energi.png" class="labo-logo" alt="">
+            </div>
+            <div class="col-lg-8">
+                <p class="labo-desc">Laboratorium ini merupakan tempat khusus untuk mempelajari dan mendalami bidang ilmu energi dan pengkondisian lingkungan (rekayasa thermal). Dalam menunjang bidang ilmu tersebut, maka laboratorium ini dibagi menjadi tiga bidang minat dalam pembelajarannya. Antara lain yaitu bidang minat Rekayasa Thermal, bidang minat Renewable Energi, dan bidang minat Energi Management dan Green Building</p>
+            </div>
+        </div>
+        <h2 class="labo-section mt-5 mb-4">Bidang pengembangan</h2>
+        <ol class="labo-list ps-4">
+            <li>Bidang Minat Rekayasa Thermal (Thermal and Fluid Engineering)</li>
+            <li>Bidang Minat Renewable Energy</li> 
+            <li>Bidang Minat Energy Management and Green Building</li>  
+        </ol>
+        <h2 class="labo-section mt-5 mb-4">Penghargaan</h2>
+        <ol class="labo-list ps-4">
+            <li>Juara 2 Lomba KTI DISCO 4th 2021</li>
+            <li>Juara 2 International Geothermal Study Case Competition by OGIP 2021</li> 
+            <li>Favorite winner Baroeyes International Energy Fair 2021</li>
+            <li>Juara 1 Lomba KTI Pekan Raya Fisika IV Nasional 2021</li> 
+            <li>4th Place Global Virtual League</li> 
+            <li>3rd Place Virtual Technical Inspection</li>
+            <li>2nd Autonomous Programming</li> 
+            <li>Winner off track award : Data and Telemetry</li>
+        </ol>
+    </div>
+    <img src="/svg/labo-wire.svg" class="labo-wire-bottom" alt="labo-wire-bottom">
+</section>
+@endsection
---
+++ resources/views/main/laboratories/ico.blade.php
@@ -0,0 +1,50 @@
+@extends("main.layouts.main")
+
+@section("content")
+<section id="laboratoire">
+    <img src="/svg/labo-wire.svg" class="labo-wire-top" alt="labo-wire-top">
+    <div class="container px-4 px-lg-0 pb-5">
+        <h1 class="labo-title text-center py-4">Laboratorium Instrumentasi, Kontrol, dan Optimisasi</h1>
+        <div class="row align-items-center">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="/img/lab-logo/ico.png" class="labo-logo" alt="">
+            </div>
+            <div class="col-lg-8">
+                <p class="labo-desc">Laboratorium instrumentasi, kontrol, dan optimisasi adalah laboratorium di departemen teknik fisika yang berfokus pada bidang instrumentasi dan pengembangannya meliputi manufacturing, desain, hingga aplikasi dari instrument dan control. Proses deteksi, observasi, pengukuran, dan akuisisi data dipelajaridalam berbagai bidang mulai dari industry hingga medis.</p>
+            </div>
+        </div>
+        <h2 class="labo-section mt-5 mb-4">Bidang pengembangan</h2>
+        <ol class="labo-list ps-4">
+            <li>Mengembangkan rancang bangun instrumentasi, kalibrasi, pengukuran, pengendalian, dan safety untuk aplikasi di industri dan medis</li>
+            <li>Mangembangkan teknologi jaringan untuk sistem deteksi, observasi, dan pengendalian.</li> 
+            <li>Mengembangkan metode neural network, genetic algoritma, fuzzy logic, artificial intelligent</li> 
+            <li>Mengembangkan rancang bangun instrumentasi kedokteran untuk aplikasi medis</li> 
+        </ol>
+        <h2 class="labo-section mt-5 mb-4">Penelitian</h2>
+        <ol class="labo-list ps-4">
+            <li>Utami, E & Purnomo, M & Rizki, R & Biyanto, Totok. (2020). Preliminary sandstone reservoir depth prediction with pre-processing data using principle component analysis (PCA) and partial least square (PLS) based on well logging.</li>
+            <li>Purnomo, Leo & Biyanto, Totok. (2019). Neural network model for profile temperature of nickel kiln. AIP Conference Proceedings. 2088. 020015. 10.1063/1.5095267</li> 
+        </ol>
+        <h2 class="labo-section mt-5 mb-4">Penghargaan</h2>
+        <ol class="labo-list ps-4">
+            <li>1st winner paper competition and petroolympic case study competition oil and gas horizons XI at gubkin university, Russia.</li>
+            <li>1st place (champion) plant design competition energy and environmental competition 2019 at university teknologi mara, Malaysia.</li> 
+            <li>2nd (runner up) power plant design competition SPE UTM</li>
+            <li>2nd RU paper competition (oil and gas festival 2018 malaysia)</li> 
+            <li>3rd winner of international process control competition 2018 at institute teknologi sepuluh nopember (ITS)</li> 
+        </ol>
+        <div class="row align-items-center mt-5 labo-img">
+            <div class="col-md-4 mb-4">
+                <img src="/img/ico-jpg/ico-1.jpg" class="ico-1" alt="ico-1">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img src="/img/ico-jpg/ico-2.jpg" class="ico-1" alt="ico-1">
+            </div>
+            <div class="col-md-4 mb-4">
+                <img src="/img/ico-jpg/ico-3.jpg" class="ico-1" alt="ico-1">
+            </div>
+        </div>
+    </div>
+    <img src="/svg/labo-wire.svg" class="labo-wire-bottom" alt="labo-wire-bottom">
+</section>
+@endsection
